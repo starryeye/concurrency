5c91491cb63140ba2a9b1f7e6bc490d9c3380754
starryeye <see2818@naver.com>
2023-07-14 23:21:14 +0900

    [starryeye, 2023.07.14] - 동시성 문제 해결을 위한 synchronized 키워드
    내용
    - 동시성 문제 해결을 위한 synchronized 키워드

--- stock/src/main/java/dev/practice/stock/domain/Stock.java
+++ stock/src/main/java/dev/practice/stock/domain/Stock.java
@@ -33,13 +33,6 @@ public class Stock {
      * - JPA 쓰기 지연으로.. decreaseStock 메서드가 종료 될 때 DB 에 반영된다.
      * - 하지만, synchronized 키워드는 decrease 메서드에 적용되어 있으므로..
      * - decrease 메서드 종료와 decreaseStock 메서드 종료 시점 사이의 찰나의 순간에 동시성 문제가 생겨버린다.
-     *
-     * 참고
-     * - synchronized 키워드를 붙이고 동시성 문제를 해결하고 싶다면 decreaseStock 메서드에 synchronized 키워드를 붙이고
-     * - @Transactional 어노테이션을 제거 해야한다.
-     * (JPA 쓰기 지연은 메서드 종료후 수행되므로 제거 하지 않으면 메서드 종료 시점과 쓰기지연 시점 사이에 동시성 문제가 발생한다.)
-     * - @Transactional 어노테이션이 제거 되었기 때문에 쓰기 지연이 동작하지 못하므로 stockRepository.saveAndFlush(stock); 를 추가해야한다.
-     * -> StockServiceWithSynchronized 에 구현
      */
     public synchronized void decrease(Long quantity) {
 
--- stock/src/main/java/dev/practice/stock/service/StockServiceWithSynchronized.java
+++ stock/src/main/java/dev/practice/stock/service/StockServiceWithSynchronized.java
@@ -5,16 +5,23 @@ import dev.practice.stock.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-/**
- * 참고
- * - synchronized 키워드는 단일 JVM 에서만 동작하므로 분산 환경에서는 여전히 동시성 문제가 발생한다.
- */
 @Service
 @RequiredArgsConstructor
 public class StockServiceWithSynchronized {
 
     private final StockRepository stockRepository;
 
+    /**
+     * 동시성 문제 해결하기 위해 StockService 와 비교하여 달라진 점.
+     * - 동시성 문제 해결을 위해 synchronized 키워드를 붙였다.
+     * - @Transactional 어노테이션을 제거했다.
+     * (JPA 쓰기 지연은 메서드 종료후 수행되므로 제거 하지 않으면 메서드 종료 시점과 쓰기지연 시점 사이에 동시성 문제가 발생한다.)
+     * - @Transactional 어노테이션이 제거 되었기 때문에 쓰기 지연이 동작하지 못하므로 stockRepository.saveAndFlush(stock); 를 추가해야한다.
+     * (synchronized 범위내에 commit 이 이루어지므로 동시성 문제가 발생하지 않는다.)
+     * 
+     * 참고
+     * - synchronized 키워드는 단일 JVM 에서만 동작하므로 분산 환경에서는 여전히 동시성 문제가 발생한다.
+     */
     public synchronized void decreaseStock(Long id, Long quantity) {
         /**
          * 1. 재고 조회
