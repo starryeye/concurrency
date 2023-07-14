ae21004f47c1495dbbeb97ebfbd51812813e87cb
starryeye <see2818@naver.com>
2023-07-14 20:41:17 +0900

    [starryeye, 2023.07.14] - Test 코드 작성
    내용
    - Test 코드 작성

--- stock/src/test/java/dev/practice/stock/service/StockServiceTest.java
+++ stock/src/test/java/dev/practice/stock/service/StockServiceTest.java
@@ -1,7 +1,43 @@
 package dev.practice.stock.service;
 
+import dev.practice.stock.domain.Stock;
+import dev.practice.stock.repository.StockRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class StockServiceTest {
 
+    @Autowired
+    private StockService stockService;
+
+    @Autowired
+    private StockRepository stockRepository;
+
+    @BeforeEach
+    void setUp() {
+
+        Stock stock = Stock.builder()
+                .productId(1L)
+                .quantity(100L)
+                .build();
+
+        stockRepository.save(stock);
+    }
+
+    @Transactional
+    @Test
+    void decreaseStock() {
+
+        stockService.decreaseStock(1L, 10L);
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertEquals(90L, stock.getQuantity());
+    }
 }
