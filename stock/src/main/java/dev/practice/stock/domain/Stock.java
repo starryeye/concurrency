b055bfd77b3eafafa03c9ad6b1a538440572b564
starryeye <see2818@naver.com>
2023-07-14 21:34:50 +0900

    [starryeye, 2023.07.14] - 동시성 문제 해결을 위한 synchronized 키워드
    내용
    - 동시성 문제 해결을 위한 synchronized 키워드

--- stock/src/main/java/dev/practice/stock/domain/Stock.java
+++ stock/src/main/java/dev/practice/stock/domain/Stock.java
@@ -27,7 +27,23 @@ public class Stock {
         this.quantity = quantity;
     }
 
-    public void decrease(Long quantity) {
+    /**
+     * 이 메서드에 synchronized 키워드를 붙이면 어떻게 될까?
+     * - decrease 메서드를 호출하는 메서드(decreaseStock)에 @Transactional 어노테이션이 적용되어있으므로...
+     * - JPA 쓰기 지연으로.. decreaseStock 메서드가 종료 될 때 DB 에 반영된다.
+     * - 하지만, synchronized 키워드는 decrease 메서드에 적용되어 있으므로..
+     * - decrease 메서드 종료와 decreaseStock 메서드 종료 시점 사이의 찰나의 순간에 동시성 문제가 생겨버린다.
+     *
+     * 참고
+     * - synchronized 키워드를 붙이고 동시성 문제를 해결하고 싶다면 decreaseStock 메서드에 synchronized 키워드를 붙이고
+     * - @Transactional 어노테이션을 제거 해야한다.
+     * (JPA 쓰기 지연은 메서드 종료후 수행되므로 제거 하지 않으면 메서드 종료 시점과 쓰기지연 시점 사이에 동시성 문제가 발생한다.)
+     * - @Transactional 어노테이션이 제거 되었기 때문에 쓰기 지연이 동작하지 못하므로 stockRepository.saveAndFlush(stock); 를 추가해야한다.
+     *
+     * 참고
+     * - synchronized 키워드는 단일 JVM 에서만 동작하므로 분산 환경에서는 여전히 동시성 문제가 발생한다.
+     */
+    public synchronized void decrease(Long quantity) {
 
         if(this.quantity - quantity < 0)
             throw new IllegalArgumentException("재고가 부족합니다.");
